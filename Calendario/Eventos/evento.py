e6a9299afd002d26236ae53d9b0c28bc1d6acee6
Gaston <gaston010.gv@gmail.com>
2023-03-04 15:44:12 -0300

    comit

--- Calendario/Eventos/evento.py
+++ Calendario/Eventos/evento.py
@@ -125,8 +125,6 @@ def cargar_csv(nombre, fecha, hora):
 
 cargar_csv("Cumpleaños", "12/12/2020", "12:00")
 
-print("Mi primer comit.")
-
 print("Mi segundo comit.")
 print("Mi tercer comit.")
 print("Mi cuarto comit.")
